32056583d691887572dd0befa441600bb124aeb4
Daniel Fornica <danielfornica@Amandas-MacBook-Pro.local>
2019-03-05 19:30:23 -0600

    dino

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("hi");
+const dinosaurs = [
+    {
+    dinoType: 'tricep'
+    },
+    {
+    dinoType:'trex'
+    },
+    {
+    dinoType:"mosasaurus"
+    }
+];
+
+//loop over the dinosaurs
+//build up a domstring
+//print the domstring to the dom
+
+const printToDom = (divId, textToprint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToprint;
+};
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for (let i = 0; i < dinosaurs.length; i++){
+        console.log(dinosaurs[i].dinoType);
+            domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        
+        console.log(domString);
+    }
+    printToDom('dino-barn',domString)
+};
+
+const init = () => {
+    buildDinosaurs();
+};
+
+init();
+
